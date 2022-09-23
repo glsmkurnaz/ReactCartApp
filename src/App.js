b5335a90eb04497aaf3eb0c417e80d14cc089b5f
glsmkurnaz <glsmkrnz27@gmail.com>
2022-09-24 00:32:04 +0300

    added project files

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { connect } from "react-redux";
+import "./styles.css";
+import { Route, Routes } from "react-router-dom";
+
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+
+const App = (props) => {
+  console.log(props.bookList, "App");
+  return (
+    <div className="App">
+      <h1>
+        Alışveriş Sepeti Yapımı
+        <img
+          src="https://avatars3.githubusercontent.com/u/60869810?v=4"
+          alt="React Dersleri"
+        />{" "}
+        React Dersleri
+      </h1>
+      <Routes>
+        <Route exact path="/" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    bookList: state.bookList,
+  };
+};
+
+export default connect(mapStateToProps)(App);
